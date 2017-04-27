6c7c6d05f30f194e570653522cd560a4dfcfdcdf
Bitluck <schukin_andrei@mail.ru>
2017-04-27 19:33:43 +0600

    Small change by uncommenting
    Uncommented begin and end lines of procedure of deleting in file
    'createPricesView.sql'

--- chap06-views/createPricesView.sql
+++ chap06-views/createPricesView.sql
@@ -1,11 +1,11 @@
 --CREATE VIEW WHICH WILL BE DISPLAYING TYPES OF COMFORT WITH AGGREGATED PRICES FOR EACH OF TYPE
---BEGIN
+BEGIN
   FOR i IN (SELECT view_name FROM user_views
           WHERE view_name = 'sum_price_view') LOOP
     EXECUTE IMMEDIATE 'DROP VIEW sum_price_view';
   END LOOP;
---END;
-
+END;
+/
 CREATE VIEW sum_price_view
 AS
 SELECT R.r_comfort, SUM(R.r_price) AS price
